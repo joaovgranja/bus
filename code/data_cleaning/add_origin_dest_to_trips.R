a3584833ecf7dcacd05d22f92ec3f127c9c72f25
Joao Granja <joaov.granja@gmail.com>
2020-04-18 17:12:29 -0400

    Using the recently cleaned 12_2019 and 01_2020 files.

--- code/data_cleaning/add_origin_dest_to_trips.R
+++ code/data_cleaning/add_origin_dest_to_trips.R
@@ -24,22 +24,30 @@ data_trips
 # Load all of the tickets dataset, but only the route and the route origin-dest info
 files_data_tix <- list.files( "raw_data/monitriip/bilhetes_de_passagem", full.names = TRUE )
 files_data_tix
-files_data_tix <- files_data_tix[ str_detect( files_data_tix, "venda_passagem_\\d{2}_\\d{4}.csv" ) ]
+# I want to use the _26_fields files
+# First keep only 01_2019-11_2019.
+files_data_tix <- files_data_tix[ str_detect( files_data_tix, "venda_passagem_\\d{2}_2019.csv" ) ]
+files_data_tix <- files_data_tix[ -length( files_data_tix ) ]
+# Now append the two _26_fields_files
+files_data_tix <- c( files_data_tix,
+                     "raw_data/monitriip/bilhetes_de_passagem/venda_passagem_12_2019_26_fields.csv",
+                     "raw_data/monitriip/bilhetes_de_passagem/venda_passagem_01_2020_26_fields.csv" )
+
 # Load one file just to check variable names
 tmp_tix <- fread( files_data_tix[[ 1 ]], nrows = 10 )
 names( tmp_tix )
 rm( tmp_tix )
 list_data_tix <- lapply( files_data_tix,
                          fread,
-                         select = c( "nu_linha", "origem_destino_linha" ) )
+                         select = c( "cnpj", "nu_linha", "origem_destino_linha" ) )
 str( list_data_tix, 1 ) 
 data_tix <- rbindlist( list_data_tix )
 rm( list_data_tix )
 data_tix
-data_tix[ , c( "ponto_origem_linha", "ponto_destino_linha" ) := tstrsplit( origem_destino_linha, split = " - " ) ][]
+data_tix[ , c( "ponto_origem_linha", "ponto_destino_linha" ) := tstrsplit( origem_destino_linha, split = " - " ) ][] # This takes a while
 data_tix[ , origem_destino_linha := NULL ]
 data_tix <- unique( data_tix )
-data_tix
+data_tix # 2,811 unique lines
 
 # Save the data with routes and origin/dest -------------------------------
 
@@ -50,7 +58,10 @@ fwrite( x = data_tix, file = "clean_data/dt_route_origin_dest.csv" )
 data_trips
 data_tix
 
-data_trips <- merge.data.table( x = data_trips, y = data_tix, by = "nu_linha", all.x = TRUE )
+data_trips <- merge.data.table( x = data_trips,
+                                y = data_tix,
+                                by = "nu_linha",
+                                all.x = TRUE )
 
 # Save data_trips with origin/dest ----------------------------------------
 
